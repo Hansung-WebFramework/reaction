5ac47451b3b2af3e6b061a37037b9687e2a4fb93
rokaf6444 <dino1106@naver.com>
2024-11-01 13:10:34 +0900

    chore: 리액트 버전 고정 및 초기 설정

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 모든 JS, JSX, TS, TSX 파일
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
