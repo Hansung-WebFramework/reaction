1d07d67864544426a08fd364d9cbe3cb3925da7e
sanghun <vitash1215@gmail.com>
2024-11-18 16:45:14 +0900

    chore : file path 관리

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// https://vite.dev/config/
+// ESM 환경에서 __dirname을 정의하기 위한 코드
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
